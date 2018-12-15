5cca1b106d7a4ec6136f0859472f65783b180fba
Vyacheslav <beletskyy.v@gmail.com>
2018-12-15 19:49:39 +0200

    initial commit

---
+++ src/main/java/com/company/vacation/diagram/EmailConfirmationService.java
@@ -0,0 +1,23 @@
+package com.company.vacation.diagram;
+
+import com.company.vacation.service.EmailService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailConfirmationService implements JavaDelegate {
+
+    private final EmailService emailService;
+
+    @Autowired
+    public EmailConfirmationService(EmailService emailService) {
+        this.emailService = emailService;
+    }
+    @Override
+    public void execute(DelegateExecution execution) {
+
+        emailService.sendConfirmation();
+    }
+}
